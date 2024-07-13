e87ed28b0779ff7fefec610871e4ea12e059a678
Noer Paanakker <noerpaanakker@gmail.com>
2024-07-13 10:33:47 +0200

    Setup first react query for accounts

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,31 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import useGetAccounts from "@/features/accounts/api/use-get-accounts";
 import { SignOutButton } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
 
 export default function Home() {
+  const { data, isLoading } = useGetAccounts();
+
+  if (isLoading) {
+    return (
+      <div>
+        <Loader2 className="size-8 animate-spin text-slate-400" />
+      </div>
+    );
+  }
+
   return (
     <>
       <p>This is an auth route</p>
+      <div>
+        {data?.accounts && data.accounts.length > 0
+          ? data.accounts.map((account) => (
+              <div key={account.id}>{account.name}</div>
+            ))
+          : "nothing"}
+      </div>
       <SignOutButton redirectUrl="/login">
         <Button variant="destructive">Sign out</Button>
       </SignOutButton>
