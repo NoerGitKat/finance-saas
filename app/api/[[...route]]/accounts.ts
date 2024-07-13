6518201e262c03dd24080b43dd0f776991e8e2d9
Noer Paanakker <noerpaanakker@gmail.com>
2024-07-13 17:16:52 +0200

    Added new account endpoint

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,16 +1,51 @@
 import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { insertAccountSchema } from "@/db/schema/accounts";
 
-const app = new Hono().get("/", async (context) => {
-  const data = await db
-    .select({
-      id: accounts.id,
-      name: accounts.name,
-    })
-    .from(accounts);
+const app = new Hono()
+  .get("/", clerkMiddleware(), async (context) => {
+    const auth = getAuth(context);
 
-  return context.json({ accounts: data || [] });
-});
+    if (!auth?.userId) {
+      return context.json({ error: "Unauthorized" }, 401);
+    }
+
+    const data = await db
+      .select({
+        id: accounts.id,
+        name: accounts.name,
+      })
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId));
+
+    return context.json({ accounts: data || [] }, 200);
+  })
+  .post(
+    "/",
+    clerkMiddleware(),
+    zValidator("json", insertAccountSchema.pick({ name: true })),
+    async (context) => {
+      const auth = getAuth(context);
+      const formValues = context.req.valid("json");
+
+      if (!auth?.userId) {
+        return context.json({ error: "Unauthorized" }, 401);
+      }
+
+      const [newAccount] = await db
+        .insert(accounts)
+        .values({
+          userId: auth.userId,
+          ...formValues,
+        })
+        .returning();
+
+      return context.json({ data: newAccount }, 200);
+    },
+  );
 
 export default app;
--- db/schema/accounts.ts
+++ db/schema/accounts.ts
@@ -1,11 +1,15 @@
 import { pgTable, text } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
+import { createId } from "@paralleldrive/cuid2";
 
 export const accounts = pgTable("accounts", {
-  id: text("id").primaryKey(),
+  id: text("id").primaryKey().$defaultFn(createId),
   plaidId: text("plaid_id"),
   name: text("name").notNull(),
   userId: text("user_id").notNull(),
 });
+
+export const insertAccountSchema = createInsertSchema(accounts);
 
 export type Account = typeof accounts.$inferSelect;
 export type NewAccount = typeof accounts.$inferInsert;
