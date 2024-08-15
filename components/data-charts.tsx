393744c9a1417bbc8bc602bb95e85083b0a3d486
Noer Paanakker <noerpaanakker@gmail.com>
2024-08-15 16:34:06 +0200

    Added loading states for charts

--- components/data-charts.tsx
+++ components/data-charts.tsx
@@ -3,19 +3,18 @@
 import useGetSummary from "@/features/summary/api/use-get-summary";
 import { Chart } from "./chart";
 import { Pie } from "./pie";
+import { ChartLoading } from "./chart-loading";
 
 export const DataCharts = () => {
   const { data, isLoading } = useGetSummary();
 
-  if (isLoading) return <div>loading...</div>;
-
   return (
     <section className="grid grid-cols-1 gap-8 lg:grid-cols-6">
       <div className="col-span-1 lg:col-span-3 xl:col-span-4">
-        <Chart data={data?.days} />
+        {isLoading ? <ChartLoading /> : <Chart data={data?.days} />}
       </div>
-      <div className="col-span-1 lg:col-span-3 xl:col-span-4">
-        <Pie data={data?.categories.all} />
+      <div className="col-span-1 lg:col-span-3 xl:col-span-2">
+        {isLoading ? <ChartLoading /> : <Pie data={data?.categories.all} />}
       </div>
     </section>
   );
